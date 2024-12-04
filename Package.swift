85488d25c7d0073e100e0b2f859ba18e5096684b
xueyuejie <358838943@qq.com>
2024-12-04 16:38:16 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap", from: "1.0.5"),
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.1"),
-        .package(url: "https://github.com/lishuailibertine/web3swift", from: "1.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", .upToNextMajor(from: "8.1.1")),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
         .package(url: "https://github.com/Flight-School/AnyCodable.git", .exact("0.6.1")),
@@ -35,8 +34,7 @@ let package = Package(
                            "PromiseKit",
                            "BigInt",
                            "AnyCodable",
-                           "Alamofire",
-                           "web3swift"
+                           "Alamofire"
                           ]),
         .testTarget(
             name: "TonSwiftTests",
