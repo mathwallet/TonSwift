2c5c764a9cf46c20fc0cf2b11c16b89577e4e174
Forrest <zyyu1990@gmail.com>
2025-07-24 21:04:02 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.1"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", .upToNextMajor(from: "8.1.1")),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
-        .package(url: "https://github.com/Flight-School/AnyCodable.git", .exact("0.6.1")),
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.6.1"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1")
     ],
     targets: [
